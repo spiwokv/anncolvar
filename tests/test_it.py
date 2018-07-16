60b03a40774cb3b92d076b475b3e1922845c2fa2
Vojtech Spiwok <spiwokv@vscht.cz>
2018-07-16 15:53:58 +0200

    new lib name

--- tests/test_it.py
+++ tests/test_it.py
@@ -7,20 +7,20 @@ import datetime as dt
 import sys
 import os
 
-import anncvlib
+import anncolvar
 
 def test_it():
   myinfilename = os.path.join(os.path.dirname(__file__), 'traj_fit.xtc')
   myintopname = os.path.join(os.path.dirname(__file__), 'reference.pdb')
   mycolvarname = os.path.join(os.path.dirname(__file__), 'results_isomap')
-  ae, cor = anncvlib.anncollectivevariable(infilename=myinfilename,
-                                           intopname=myintopname,
-                                           colvarname=mycolvarname,
-                                           column=2, boxx=1.0, boxy=1.0, boxz=1.0,
-                                           atestset=0.1, shuffle=1, layers=4, layer1=16, layer2=8, layer3=4,
-                                           actfun1='sigmoid', actfun2='sigmoid', actfun3='sigmoid',
-                                           optim='adam', loss='mean_squared_error', epochs=1000, batch=256,
-                                           ofilename='', modelfile='', plumedfile='')
+  ae, cor = anncolvar.anncollectivevariable(infilename=myinfilename,
+                                            intopname=myintopname,
+                                            colvarname=mycolvarname,
+                                            column=2, boxx=1.0, boxy=1.0, boxz=1.0,
+                                            atestset=0.1, shuffle=1, layers=4, layer1=16, layer2=8, layer3=4,
+                                            actfun1='sigmoid', actfun2='sigmoid', actfun3='sigmoid',
+                                            optim='adam', loss='mean_squared_error', epochs=1000, batch=256,
+                                            ofilename='', modelfile='', plumedfile='')
   assert(cor > 0.99)
 
 if __name__ == '__main__':
