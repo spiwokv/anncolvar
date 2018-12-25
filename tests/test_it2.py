7beeafebc11e02af66069cad9a073b443fde496c
Vojtech Spiwok <spiwokv@vscht.cz>
2018-12-25 20:33:41 +0100

    Update test_it2.py

--- tests/test_it2.py
+++ tests/test_it2.py
@@ -23,7 +23,7 @@ def test_it():
                                             optim='adam', loss='mean_squared_error', epochs=1000, batch=256,
                                             ofilename='', modelfile='', plumedfile=myplumedname)
   
-  command = "plumed driver --mf_pdb "+myintopname+" --plumed "+myplumedname
+  command = "/home/travis/opt/bin/plumed driver --mf_pdb "+myintopname+" --plumed "+myplumedname
   os.system(command)
   ifile = open("COLVAR", "r").readlines()
   print(ifile)
