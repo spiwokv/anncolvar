0704286251bf75775dd2ee659a4bb337f9fb7fb7
Vojtech Spiwok <spiwokv@vscht.cz>
2019-11-06 17:04:59 +0100

    error in print

--- anncolvar/__init__.py
+++ anncolvar/__init__.py
@@ -509,7 +509,7 @@ def anncollectivevariable(infilename='', intopname='', colvarname='', column=2,
       toprint = toprint[:-1] + "\n"
       ofile.write(toprint)
       ofile.write("NUM_LAYERS=2\n")
-      ofile.write("NUM_NODES=%i,%i\n" % layer1,layer2)
+      ofile.write("NUM_NODES=%i,%i\n" % (layer1,layer2))
       if actfun1 == 'tanh' and actfun2 == 'tanh':
         ofile.write("ACTIVATIONS=Tanh,Tanh\n")
       else:
@@ -561,7 +561,7 @@ def anncollectivevariable(infilename='', intopname='', colvarname='', column=2,
       toprint = toprint[:-1] + "\n"
       ofile.write(toprint)
       ofile.write("NUM_LAYERS=2\n")
-      ofile.write("NUM_NODES=%i,%i,%i\n" % layer1,layer2,layer3)
+      ofile.write("NUM_NODES=%i,%i,%i\n" % (layer1,layer2,layer3))
       if actfun1 == 'tanh' and actfun2 == 'tanh' and actfun3 == 'tanh':
         ofile.write("ACTIVATIONS=Tanh,Tanh,Tanh\n")
       else:
