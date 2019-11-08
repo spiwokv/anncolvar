c6b386f8387010f9dad919e04fe6826b6e8dc9b6
Vojtech Spiwok <spiwokv@vscht.cz>
2019-11-08 07:57:53 +0100

    Update test_it5.py

--- tests/test_it5.py
+++ tests/test_it5.py
@@ -24,9 +24,6 @@ def test_it():
                                             optim='adam', loss='mean_squared_error', epochs=1000, batch=256,
                                             ofilename='', modelfile='', plumedfile=myplumedname, plumedfile2=myplumedname2)
   
-  ifile = open(myplumedname2, "r").readlines()
-  for line in ifile:
-    print(line)
   command = "plumed driver --mf_pdb "+myintopname+" --plumed "+myplumedname2
   os.system(command)
   ifile = open("COLVAR", "r").readlines()
