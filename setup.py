2528c6f73809542e932e66b2c35cc0cce9f51363
Vojtech Spiwok <spiwokv@vscht.cz>
2019-12-01 20:54:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
     return f.read()
 
 setup(name='anncolvar',
-      version='0.7',
+      version='0.8',
       description='Coding collective variables by artificial neural networks',
       long_description=readme(),
       classifiers=[
